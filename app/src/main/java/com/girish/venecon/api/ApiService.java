55944e9512892f0509ea3854d7f9fd761cbd6813
Vucko <vucic94@yahoo.com>
2018-03-22 15:47:31 +0100

    Making firebase notifications Token and saving to DB works Getting a message still untested properly

--- app/src/main/java/com/girish/venecon/api/ApiService.java
+++ app/src/main/java/com/girish/venecon/api/ApiService.java
@@ -15,6 +15,7 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface ApiService {
     @GET("output.php?table=ve_fx&format=json&start=2010-01-01&key=dsCHiQDZc2HvYFNYrQrMhQOczHUDHAUSHD8")
@@ -35,4 +36,6 @@ public interface ApiService {
     Call<List<CrudeProductionData>> getCrudeProductionData();
     @GET("output.php?table=ve_US&format=json&start=2002-01-01&key=dsCHiQDZc2HvYFNYrQrMhQOczHUDHAUSHD8")
     Call<List<UsOilData>> getUsOilData();
+    @GET("notifications.php")
+    Call<Void> saveToken(@Query("id") String refreshedToken, @Query("lan") String languageCode);
 }
