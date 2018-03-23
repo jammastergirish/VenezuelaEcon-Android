fd4cf3debb17acecc56ec1444e4941acbb4b3abb
Vucko <vucic94@yahoo.com>
2018-03-23 13:26:14 +0100

    Initial setup for IAP, still untested

--- app/src/main/java/com/girish/venecon/MainActivity.java
+++ app/src/main/java/com/girish/venecon/MainActivity.java
@@ -2,7 +2,13 @@ package com.girish.venecon;
 
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.os.RemoteException;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -10,10 +16,14 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 
+import com.android.vending.billing.IInAppBillingService;
 import com.girish.venecon.utils.Constants;
 import com.google.android.gms.ads.MobileAds;
+
+import java.util.ArrayList;
 
 import static com.girish.venecon.Utils.mContext;
 //import com.google.firebase.analytics.FirebaseAnalytics;
@@ -24,6 +34,21 @@ public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     //private FirebaseAnalytics mFirebaseAnalytics;
+    IInAppBillingService mService;
+
+    ServiceConnection mServiceConn = new ServiceConnection() {
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mService = null;
+        }
+
+        @Override
+        public void onServiceConnected(ComponentName name,
+                                       IBinder service) {
+            mService = IInAppBillingService.Stub.asInterface(service);
+//            queryItems();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,9 +72,32 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+        initializeInAppPurchases();
         changeToFragment(new FXFragment(), mContext.getString(R.string.foreign_exchange));
         MobileAds.initialize(this, Constants.ADMOB_APP_ID);
 
+    }
+
+    private void queryItems() {
+        ArrayList<String> skuList = new ArrayList<String>();
+//        skuList.add("premium_upgrade");
+//        skuList.add("gas");
+        Bundle querySkus = new Bundle();
+        querySkus.putStringArrayList("ITEM_ID_LIST", skuList);
+        try {
+            Bundle skuDetails = mService.getSkuDetails(3,
+                    getPackageName(), "inapp", querySkus);
+            Log.i("sm", "sad");
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void initializeInAppPurchases() {
+        Intent serviceIntent =
+                new Intent("com.android.vending.billing.InAppBillingService.BIND");
+        serviceIntent.setPackage("com.android.vending");
+        getApplicationContext().bindService(serviceIntent, mServiceConn, Context.BIND_AUTO_CREATE);
     }
 
     @Override
@@ -141,6 +189,14 @@ public class MainActivity extends AppCompatActivity
                 .replace(R.id.content_frame
                         , fragment)
                 .commit();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mService != null) {
+            unbindService(mServiceConn);
+        }
     }
 }
 
